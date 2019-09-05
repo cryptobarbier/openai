104eb4c0789a77ba4adeadfd78768e41d1a5eea1
cryptobarbier <54747784+cryptobarbier@users.noreply.github.com>
2019-09-05 20:10:16 +0100

    Add files via upload

--- sportify.py
+++ sportify.py
@@ -41,20 +41,22 @@ class Sportify(gym.Env):
 
         self.odds=1/(self.np_random.randint(1,99)/100)
         self.minutes=self.np_random.randint(10,90)
+        
         if self.training==1:
             self.match_id=sample(self.train_id,1)[0]
             self.outcome=self.df_train[self.df_train['match_id']==self.match_id]['A Winner'].iloc[0]
-            dfobs=self.df_train[(self.df_train['match_id']==self.match_id)&(self.df_train['minutes']==self.minutes)]
+            dfobs=self.df_train.fillna(0)
             dfobs['Odds']=self.odds
-            self.observation=dfobs[self.features].drop(['A Winner','match_id'],axis=1)
+            dfobs=dfobs[(dfobs['match_id']==self.match_id)&(dfobs['minutes']==self.minutes)]
+            self.observation=dfobs.drop(['A Winner','match_id'],axis=1)
             
         else:
             self.match_id=sample(self.test_id,1)[0]
             self.outcome=self.df_test[self.df_test['match_id']==self.match_id]['A Winner'].iloc[0]
-            dfobs=self.df_test[(self.df_test['match_id']==self.match_id)&(self.df_test['minutes']==self.minutes)]
+            dfobs=self.df_test.fillna(0)
             dfobs['Odds']=self.odds
-            self.observation=dfobs[self.features].drop(['A Winner','match_id'],axis=1)
-        
+            dfobs=dfobs[(dfobs['match_id']==self.match_id)&(dfobs['minutes']==self.minutes)]
+            self.observation=dfobs.drop(['A Winner','match_id'],axis=1)
         return self.observation# extract the sample from file (training or testing)
         # etract outcome
     
